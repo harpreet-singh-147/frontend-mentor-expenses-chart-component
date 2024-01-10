8c35fccb3acd49ab1cf954f2397c4565c1f312e7
harpreet-singh-147 <h_singh_007@hotmail.co.uk>
2024-01-10 00:14:55 +0000

    initial commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='attribution'>
+      Challenge by{' '}
+      <a
+        href='https://www.frontendmentor.io?ref=challenge'
+        target='_blank'
+        aria-label='Visit Frontend Mentor Website'
+        rel='noopener noreferrer'
+        referrerPolicy='no-referrer'
+      >
+        Frontend Mentor
+      </a>
+      . <br />
+      Coded by{' '}
+      <a
+        href='https://github.com/harpreet-singh-147'
+        aria-label='More about Harpreet Singh'
+        target='_blank'
+        rel='noopener noreferrer'
+        referrerPolicy='no-referrer'
+      >
+        Harpreet Singh.
+      </a>
+    </footer>
+  );
+};
+export default Footer;
